ba028a725c840397b35c10e2342f089982cf0ce1
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-03-13 09:43:12 +0100

    Modified pytorch version in install script

--- install.sh
+++ install.sh
@@ -3,7 +3,8 @@
 echo ""
 echo ""
 echo "****************** Installing pytorch with cuda10 ******************"
-conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
+#conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
 
 echo ""
 echo ""
