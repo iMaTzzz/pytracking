0ee8946b26235e1c5914a23163a2db692d13f4a2
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-03-15 12:46:29 +0100

    Upgrade to latest pytorch version

--- install.sh
+++ install.sh
@@ -5,7 +5,8 @@ echo ""
 echo "****************** Installing pytorch with cuda10 ******************"
 #conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
 #pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
-pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+#pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
+pip install torch==2.2.0 torchvision==0.17.0 torchaudio==2.2.0 --index-url https://download.pytorch.org/whl/cu121
 
 echo ""
 echo ""
