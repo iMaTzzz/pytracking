153232e020a1a600827f017b06d617c26f0792be
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-03-15 11:21:32 +0100

    Removed torchaudio from install.sh

--- install.sh
+++ install.sh
@@ -4,7 +4,7 @@ echo ""
 echo ""
 echo "****************** Installing pytorch with cuda10 ******************"
 #conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
-pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 torchaudio==0.13.1 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
 
 echo ""
 echo ""
