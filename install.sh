ec100698f736b01e3d577c63ba1a5cd49145872d
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-03-15 12:16:39 +0100

    Downgrade cuda116 to cuda 113

--- install.sh
+++ install.sh
@@ -4,7 +4,8 @@ echo ""
 echo ""
 echo "****************** Installing pytorch with cuda10 ******************"
 #conda install -y pytorch torchvision cudatoolkit=10.0 -c pytorch
-pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
+#pip install torch==1.13.1+cu116 torchvision==0.14.1+cu116 --extra-index-url https://download.pytorch.org/whl/cu116
+pip install torch==1.12.1+cu113 torchvision==0.13.1+cu113 torchaudio==0.12.1 --extra-index-url https://download.pytorch.org/whl/cu113
 
 echo ""
 echo ""
