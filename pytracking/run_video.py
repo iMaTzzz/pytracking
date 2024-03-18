cdfa2c63d75d505b63fbf27930247f14f98eb52b
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-03-18 12:16:14 +0100

    Added output video file parameter as well as changed parameter names for clarification

--- pytracking/run_video.py
+++ pytracking/run_video.py
@@ -9,29 +9,34 @@ if env_path not in sys.path:
 from pytracking.evaluation import Tracker
 
 
-def run_video(tracker_name, tracker_param, videofile, optional_box=None, debug=None, save_results=False):
-    """Run the tracker on your webcam.
+def run_video(tracker_name, tracker_param, input_videofile, output_videofile=None, optional_box=None, debug=None, save_results=False):
+    """Run the tracker on a video.
     args:
         tracker_name: Name of tracking method.
         tracker_param: Name of parameter file.
+        input_videofile: Path to the input video (mp4)
+        output_videofile: Path to the output video (mp4)
+        optional_bbox: Path to the bounding boxes (txt)
         debug: Debug level.
+        save_results: Bool if we want to save the predictions
     """
     tracker = Tracker(tracker_name, tracker_param)
-    tracker.run_video_generic(videofilepath=videofile, optional_box=optional_box, debug=debug, save_results=save_results)
+    tracker.run_video_generic(input_videofilepath=input_videofile, ouput_videofilepath=output_videofile, optional_box=optional_box, debug=debug, save_results=save_results, ui=ui)
 
 def main():
-    parser = argparse.ArgumentParser(description='Run the tracker on your webcam.')
+    parser = argparse.ArgumentParser(description='Run the tracker on a video.')
     parser.add_argument('tracker_name', type=str, help='Name of tracking method.')
     parser.add_argument('tracker_param', type=str, help='Name of parameter file.')
-    parser.add_argument('videofile', type=str, help='path to a video file.')
-    parser.add_argument('--optional_box', type=float, default=None, nargs="+", help='optional_box with format x y w h.')
+    parser.add_argument('input_videofile', type=str, help='path to the input video.')
+    parser.add_argument('--output_videofile', type=str, help='path to the saved video.')
+    parser.add_argument('--optional_bbox', type=str, help='path to the file containing bounding boxes with format x y w h.')
     parser.add_argument('--debug', type=int, default=0, help='Debug level.')
     parser.add_argument('--save_results', dest='save_results', action='store_true', help='Save bounding boxes')
     parser.set_defaults(save_results=False)
 
     args = parser.parse_args()
 
-    run_video(args.tracker_name, args.tracker_param,args.videofile, args.optional_box, args.debug, args.save_results)
+    run_video(args.tracker_name, args.tracker_param, args.input_videofile, args.output_videofile, args.optional_box, args.debug, args.save_results, args.ui)
 
 
 if __name__ == '__main__':
