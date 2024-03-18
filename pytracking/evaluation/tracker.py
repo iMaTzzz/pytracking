2cc518d387f597187cbba45d2bfac216c909b412
Matteo Sciascia <Matteo.Sciascia2@gmail.com>
2024-03-18 15:43:41 +0100

    Replaced UI by simple selectROIs function

--- pytracking/evaluation/tracker.py
+++ pytracking/evaluation/tracker.py
@@ -287,67 +287,8 @@ class Tracker:
         else:
             raise ValueError('Unknown multi object mode {}'.format(multiobj_mode))
 
-        class UIControl:
-            def __init__(self, curr_object_id, init_bbox, init_object_ids, object_ids):
-                self.mode = "init" # Two modes: init or select
-                self.target_tl = (-1, -1)
-                self.target_br = (-1, -1)
-                self.curr_object_id = curr_object_id
-                self.init_bbox = init_bbox
-                self.init_object_ids = init_object_ids
-                self.object_ids = object_ids
-
-            def mouse_callback(self, event, x, y, flags, param):
-                global frame_copy
-                if event == cv.EVENT_LBUTTONDOWN and self.mode == 'init':
-                    self.target_tl = (x, y)
-                    self.mode = 'select'
-                #elif event == cv.EVENT_MOUSEMOVE and self.mode == 'select':
-                    #self.target_br = (x, y)
-                elif event == cv.EVENT_LBUTTONUP and self.mode == 'select':
-                    self.target_br = (x, y)
-                    self.init_object_ids.append(self.curr_object_id)
-                    self.object_ids.append(self.curr_object_id)
-                    self.init_bbox[self.curr_object_id] = self.get_bb()
-                    self.curr_object_id += 1
-                    self.mode = 'init'
-                    self.target_tl = (-1, -1)
-                    self.target_br = (-1, -1)
-                #elif event == cv.EVENT_MBUTTONUP:
-                    ## Erase last object to track
-                    #if self.curr_object_id > 0:
-                        #self.init_bbox.popitem(last=True)
-                        #self.init_object_ids.pop()
-                        #self.object_ids.pop()
-                        #self.curr_object_id -= 1
-                        #self.target_tl = (-1, -1)
-                        #self.target_br = (-1, -1)
-                        #self.mode = 'init'
-                        #self.draw_bb()
-
-            def get_tl(self):
-                tl = self.target_tl
-                br = self.target_br
-                return (min(tl[0], br[0]), min(tl[1], br[1]))
-
-            def get_br(self):
-                tl = self.target_tl
-                br = self.target_br
-                return (max(tl[0], br[0]), max(tl[1], br[1]))
-
-            def get_bb(self):
-                tl = self.target_tl
-                br = self.target_br
-                return [min(tl[0], br[0]), min(tl[1], br[1]), abs(br[0] - tl[0]), abs(br[1] - tl[1])]
-            
-            #def draw_bb(self):
-                ##self.current_image = self.original_image.copy()
-                #for box in init_bbox:
-                    #cv.rectangle(frame_copy, (box[0], box[1]), ((box[1] + box[3]), (box[2] + box[4])), (255, 0, 0), 2)
-
         display_name = 'Display: ' + self.name
-        cv.namedWindow(display_name, cv.WINDOW_NORMAL | cv.WINDOW_KEEPRATIO)
-        cv.resizeWindow(display_name, 960, 720)
+        cv.namedWindow(display_name, cv.WINDOW_FULLSCREEN)
 
         # open input video file
         assert os.path.isfile(input_video), "Invalid param {}".format(input_video)
@@ -384,23 +325,14 @@ class Tracker:
         else: # Second method: define them using the UI
             frame_copy = frame.copy()
             cv.imshow(display_name, frame_copy)
-            # Put text
-            font_color = (255, 255, 255)
-            msg = "Select target(s). Press 'd' when you are done or 'q' if you want to quit"
-            #msg = "Select target(s). Press 'd' when you are done."
-            ui_control = UIControl(curr_object_id=curr_object_id, init_bbox=init_bbox, init_object_ids=init_object_ids, object_ids=object_ids)
-            cv.setMouseCallback(display_name, ui_control.mouse_callback)
-            while True:
-                # Draw rectangles based on UIControl state
-                for box in ui_control.init_bbox.values():
-                    cv.rectangle(frame_copy, (box[0], box[1]), ((box[0] + box[2]), (box[1] + box[3])), (255, 0, 0), 2)
-                # create bounding boxes until "d" key is pressed when you are done
-                key = cv.waitKey(1000) & 0xFF
-                init_object_ids, object_ids, init_bbox = ui_control.init_object_ids, ui_control.init_object_ids, ui_control.init_bbox
-                if key == ord("d") and len(init_object_ids) != 0:
-                    break
-                elif key == ord("q"):
-                    sys.exit("Pressed 'q', exiting program")
+            rois = cv.selectROIs(display_name, frame_copy)
+            if not rois:
+                sys.exit("Pressed 'q', exiting program")
+            for index, roi in enumerate(rois, start=1):
+                init_object_ids.append(index)
+                object_ids.append(index)
+                init_bbox[index] = roi
+
             print(f"{init_object_ids=}, {object_ids=}, {init_bbox=}")
         # Close all windows
         cv.destroyAllWindows()
